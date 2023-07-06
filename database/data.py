2174ee5a7808d5461f94ee12e13fdd893f69ca3a
Valentin Cuzin <valentin.cuzin-rambaud@etu.univ-lyon1.fr>
2023-07-06 18:22:52 +0200

    Big Changes : Flask --> FastAPI asyncrous server and acces to the database Modifications qui seront validées : 	modifié :         database/data.py 	modifié :         main.py 	modifié :         templates/markdown.html

--- database/data.py
+++ database/data.py
@@ -1,27 +1,48 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
+from fastapi import Form
 from sqlalchemy import text
+from databases import Database
+from asyncpg.exceptions import PostgresError
 import json
 
-def initConnection(app : Flask):
+db: Database = None
+
+async def initConnection():
     """ initialise the connection at the database """
     with open('database/config.json') as configConnection:
-        connect = json.load(configConnection)
+        connectInfo : json = json.load(configConnection)
+    DATABASE_URL : str = f'postgresql://{connectInfo["user"]}:{connectInfo["password"]}@{connectInfo["host"]}:{connectInfo["port"]}/{connectInfo["database"]}'
     global db
-    db = SQLAlchemy()
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{connect["user"]}:{connect["password"]}@{connect["host"]}:{connect["port"]}/{connect["database"]}'
-    db.init_app(app)
+    db = Database(DATABASE_URL)
+    await db.connect()
+    await testDatabaseConnection()
 
-def sendFormConnection(request_ : request):
+async def testDatabaseConnection():
+    """ test the connection at the database """
+    try:
+        query = text("SELECT 1")
+        await db.fetch_all(query)
+        print("Database connection successful!")
+    except Exception as e: 
+        print("Error connecting to the database:", e)
+        raise
+
+async def closeDatabaseConnection():
+    """ disconnect at the database """
+    await db.disconnect()
+
+async def sendFormConnection(form_ : Form):
     """ Send request for connection """
-    mail : str = request_.form['mail']
-    password : str = request_.form['password']
+    mail : str = form_['mail']
+    password : str = form_['password']
     try : 
         query : text = text(f"SELECT * FROM Account WHERE email = '{mail}' AND password = crypt('{password}', password); ")
         print(query)
-        results = db.session.execute(query)
-    except :
-        print(f"erreur, mauvaise email ou mot de passe")
-        return
-    print(f"voici les infos du comptes {results.all()}")
-    return results
+        results = await db.fetch_one(query)
+        if results is None:
+            print("bad email or password...")
+        else:
+            print(f"voici les infos du comptes {dict(results)} : ")
+    except PostgresError as e:
+        print(f"Error from the database : {str(e)}")
+    except Exception as e:
+        print(f"Error : {str(e)}")
